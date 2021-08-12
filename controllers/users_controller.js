9a626a51991ae179b847edbb6e763622fe2eb758
sam <syedsamer456@gmail.com>
2021-08-12 15:00:13 +0530

    Connected user to DB

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user')
+
 module.exports.profile = function(req, res){
     return res.render('profile', {
         title: "Sam"
@@ -20,7 +22,22 @@ module.exports.signIn = function(req, res){
 
 //get signup data
 module.exports.create = function(req,res){
-    //todo
+    if (req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('Error in finding user in signing up'); return}
+
+        if (!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('Error in creating user while signing up'); return}
+                
+                return res.redirect('/users/sign-in');
+            })
+        } else{
+            return res.redirect('back');
+        }
+    })
 }
 
 //get sigin data

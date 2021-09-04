80404bc6bbecbb90e1d9f061f7ecd749f62bc2cb
sam <syedsamer456@gmail.com>
2021-09-04 14:33:28 +0530

    delete comments and posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post')
+const User = require('../models/user');
 
 module.exports.home = function(req, res){
     console.log(req.cookies);
@@ -22,10 +23,15 @@ module.exports.home = function(req, res){
         }
     })
     .exec(function(err, posts){
-        return res.render('home', {
-            title: " Convvey | Home", 
-            posts: posts
-         });
+
+        User.find({}, function(err, users){
+            return res.render('home', {
+                title: " Convvey | Home", 
+                posts: posts,
+                all_users: users
+             });
+        })
+
     })
 }
 

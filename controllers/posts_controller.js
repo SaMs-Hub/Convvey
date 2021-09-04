4143c9078f08615ed35fc585aea1714e750f9faf
sam <syedsamer456@gmail.com>
2021-09-04 22:06:51 +0530

    change comments

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -3,22 +3,33 @@ const Comment = require('../models/comment');
 
 module.exports.create = async function(req, res){
     try{
-        await Post.create({
+        let post = await Post.create({
             content: req.body.content,
             user: req.user._id
         });
         
-        req.flash('success', 'Post published');
+        if (req.xhr){
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "Post created!"
+            });
+        }
+
+        req.flash('success', 'Post published!');
         return res.redirect('back');
 
     }catch(err){
-        req.flash("Error", err);
+        req.flash('error', err);
         return res.redirect('back');
-    } 
-
+    }
+  
 }
 
+
 module.exports.destroy = async function(req, res){
+
     try{
         let post = await Post.findById(req.params.id);
 
@@ -26,16 +37,18 @@ module.exports.destroy = async function(req, res){
             post.remove();
 
             await Comment.deleteMany({post: req.params.id});
-            req.flash('success', "Post and associated comments deleted");
-              return res.redirect('back');
+
+            req.flash('success', 'Post and associated comments deleted!');
+
+            return res.redirect('back');
         }else{
-            req.flash('error', "You can't delete this post")
+            req.flash('error', 'You cannot delete this post!');
             return res.redirect('back');
         }
 
     }catch(err){
-        req.flash("Error", err);
-        return res.redirect('back'); 
+        req.flash('error', err);
+        return res.redirect('back');
     }
     
 }
